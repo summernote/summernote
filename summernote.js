56024643321cdd0c975d9293b1210beed52fbf68
Youngteac Hong <susukang98@gmail.com>
2013-05-14 20:32:23 +0900

    refactoring: use $.fn.extend

--- summernote.js
+++ summernote.js
@@ -387,48 +387,38 @@
   var renderer = new Renderer();
   var eventHandler = new EventHandler();
 
-  /**
-   * summernote 
-   *
-   * create Editor Layout and attach Key and Mouse Event
-   */
-  $.fn.summernote = function(options) {
-    options = options || {};
-    
-    // createLayout
-    renderer.createLayout(this, options.height);
-    
-    var info = renderer.layoutInfo(this);
-    eventHandler.attach(info);
-    
-    if(options.focus) { info.editable.focus(); } // options focus
-  };
-  
-  /**
-   * code
-   *
-   * get the HTML contents of note or set the HTML contents of note.
-   */
-  $.fn.code = function(sHTML) {
-    var info = renderer.layoutInfo(this);
-    
-    //get the HTML contents
-    if (sHTML === undefined) {
-      return info.editable.html();
+  //extend jquery fn
+  $.fn.extend({
+    // create Editor Layout and attach Key and Mouse Event
+    summernote : function(options) {
+      options = options || {};
+
+      // createLayout
+      renderer.createLayout(this, options.height);
+
+      var info = renderer.layoutInfo(this);
+      eventHandler.attach(info);
+
+      if(options.focus) { info.editable.focus(); } // options focus
+    },
+    // get the HTML contents of note or set the HTML contents of note.
+    code : function(sHTML) {
+      var info = renderer.layoutInfo(this);
+
+      //get the HTML contents
+      if (sHTML === undefined) {
+        return info.editable.html();
+      }
+
+      // set the HTML contents
+      info.editable.html(sHTML);
+    },
+    // destory Editor Layout and dettach Key and Mouse Event
+    destory : function() {
+      var info = renderer.layoutInfo(this);
+      eventHandler.dettach(info);
+      renderer.removeLayout(this);
     }
-    
-    // set the HTML contents
-    info.editable.html(sHTML);
-  };
-  
-  /**
-   * finish
-   */
-  $.fn.destory = function() {
-    var info = renderer.layoutInfo(this);
-    eventHandler.dettach(info);
-    renderer.removeLayout(this);
-  };
-  
+  });
 // jQuery
 })(jQuery);
