d9278f4ff4d0c28c792ccaf3a78641d120a671c5
Youngteac Hong <susukang98@gmail.com>
2013-07-16 19:54:06 +0900

    Add list function tests.

--- test/list.spec.js
+++ test/list.spec.js
@@ -5,19 +5,34 @@
  */
 var list = $.fn.summernoteInner().list;
 
-test('list.compact', function() {
-  deepEqual(list.compact([0, 1, false, 2, '', 3]), [1,2,3],
-            'falsey values of `array` removed');
+test('list.head', function() {
+  deepEqual(list.head([1, 2, 3]), 1, 'should return the first element');
+});
+
+test('list.last', function() {
+  deepEqual(list.last([1, 2, 3]), 3, 'should return the last element');
+});
+
+test('list.initial', function() {
+  deepEqual(list.initial([1, 2, 3]), [1, 2], 'should exclude last element');
+});
+
+test('list.tail', function() {
+  deepEqual(list.tail([1, 2, 3]), [2, 3], 'should exclude first element');
 });
 
 test('list.clusterBy', function() {
   var aaClustered = list.clusterBy([1, 1, 2, 2, 3], function(itemA, itemB) {
     return itemA === itemB;
   });
-  deepEqual([[1, 1], [2, 2], [3]], aaClustered, 'clusterBy equality 1');
+  deepEqual([[1, 1], [2, 2], [3]], aaClustered, 'should cluster by equality 1');
   
   var aaClustered = list.clusterBy([1, 2, 2, 1, 3], function(itemA, itemB) {
     return itemA === itemB;
   });
-  deepEqual([[1], [2, 2], [1], [3]], aaClustered, 'clusterBy equality 2');
+  deepEqual([[1], [2, 2], [1], [3]], aaClustered, 'should cluster by equality 2');
+});
+
+test('list.compact', function() {
+  deepEqual(list.compact([0, 1, false, 2, '', 3]), [1,2,3], 'falsey values of `array` removed');
 });
