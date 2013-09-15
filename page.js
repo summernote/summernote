15b8b0bf3f7f775b26c2aad657a038a5cc30f504
Alan Hong <susukang98@gmail.com>
2013-09-15 15:37:38 +0900

    scrollspy and affix

---
+++ page.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  // scrollspy  
+  var $body = $(document.body), $navbar = $('.bs-page-navbar');
+  $body.scrollspy({
+    target: '.bs-page-sidebar',
+    offset: $navbar.height()
+  });
+
+  // sidebar affix
+  var $sidebar = $('.bs-page-sidebar');
+  $sidebar.affix({
+    offset: {
+      top: function() {
+        var offsetTop = $sidebar.offset().top;
+        var nPadding = 20;
+        return (this.top = offsetTop - nPadding - $navbar.height());
+      }, bottom: function() {
+        return (this.bottom = $('.bs-page-footer').outerHeight(true));
+      }
+    } 
+  });
+});
