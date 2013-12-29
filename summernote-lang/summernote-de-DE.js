958e1bff6b460cdadf7ae2ac1541ff0a4bc2dbb9
hackerwins <susukang98@gmail.com>
2013-12-29 19:00:24 +0900

    Update docs: lang.

---
+++ summernote-lang/summernote-de-DE.js
@@ -0,0 +1,102 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'de-DE': {
+      font: {
+        bold: 'Fett',
+        italic: 'Kursiv',
+        underline: 'Unterstreichen',
+        strike: 'Durchgestrichen',
+        clear: 'Zurücksetzen',
+        height: 'Zeilenhöhe',
+        size: 'Schriftgröße'
+      },
+      image: {
+        image: 'Grafik',
+        insert: 'Grafik einfügen',
+        resizeFull: 'Originalgröße',
+        resizeHalf: 'Größe 1/2',
+        resizeQuarter: 'Größe 1/4',
+        floatLeft: 'Linksbündig',
+        floatRight: 'Rechtsbündig',
+        floatNone: 'Kein Textfluss',
+        dragImageHere: 'Ziehen Sie ein Bild mit der Maus hierher',
+        selectFromFiles: 'Wählen Sie eine Datei aus',
+        url: 'Grafik URL'
+      },
+      link: {
+        link: 'Link',
+        insert: 'Link einfügen',
+        unlink: 'Link entfernen',
+        edit: 'Editieren',
+        textToDisplay: 'Anzeigetext',
+        url: 'Ziel des Links?'
+      },
+      video: {
+        video: 'Video',
+        videoLink: 'Video Link',
+        insert: 'Insert Video',
+        url: 'Video URL?',
+        providers: '(YouTube, Vimeo, Vine, Instagram, or DailyMotion)'
+      },
+      table: {
+        table: 'Tabelle'
+      },
+      hr: {
+        insert: 'Eine horizontale Linie einfügen'
+      },
+      style: {
+        style: 'Stil',
+        normal: 'Normal',
+        blockquote: 'Zitat',
+        pre: 'Quellcode',
+        h1: 'Überschrift 1',
+        h2: 'Überschrift 2',
+        h3: 'Überschrift 3',
+        h4: 'Überschrift 4',
+        h5: 'Überschrift 5',
+        h6: 'Überschrift 6'
+      },
+      lists: {
+        unordered: 'Aufzählung',
+        ordered: 'Nummerieung'
+      },
+      options: {
+        help: 'Hilfe',
+        fullscreen: 'Vollbild',
+        codeview: 'HTML-Code anzeigen'
+      },
+      paragraph: {
+        paragraph: 'Absatz',
+        outdent: 'Einzug vergrößern',
+        indent: 'Einzug verkleinern',
+        left: 'Links ausrichten',
+        center: 'Zentriert ausrichten',
+        right: 'Rechts ausrichten',
+        justify: 'Blocksatz'
+      },
+      color: {
+        recent: 'Letzte Farbe',
+        more: 'Mehr Farben',
+        background: 'Hintergrundfarbe',
+        foreground: 'Schriftfarbe',
+        transparent: 'Transparenz',
+        setTransparent: 'Transparenz setzen',
+        reset: 'Zurücksetzen',
+        resetToDefault: 'Auf Standard zurücksetzen'
+      },
+      shortcut: {
+        shortcuts: 'Tastenkürzel',
+        close: 'Schließen',
+        textFormatting: 'Textformatierung',
+        action: 'Aktion',
+        paragraphFormatting: 'Absatzformatierung',
+        documentStyle: 'Dokumentenstil'
+      },
+      history: {
+        undo: 'Rückgängig',
+        redo: 'Wiederholen'
+      }
+
+    }
+  });
+})(jQuery);
