7aa23d1fcc91288e6469bfeeb77feab18f8fb797
babarizbak <fabien.vayssiere@gdp.fr>
2014-01-03 11:15:39 +0100

    Create summernote-fr-FR.js

---
+++ lang/summernote-fr-FR.js
@@ -0,0 +1,102 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'fr-FR': {
+      font: {
+        bold: 'Gras',
+        italic: 'Italique',
+        underline: 'Souligné',
+        strike: 'Barré',
+        clear: 'Effacer la mise en forme',
+        height: 'Interligne',
+        size: 'Taille de police'
+      },
+      image: {
+        image: 'Image',
+        insert: 'Insérer une image',
+        resizeFull: 'Taille originale',
+        resizeHalf: 'Redimensionner à 50 %',
+        resizeQuarter: 'Redimensionner à 25 %',
+        floatLeft: 'Aligné à gauche',
+        floatRight: 'Aligné à droite',
+        floatNone: 'Pas d\'alignement',
+        dragImageHere: 'Faites glisser une image avec la souris dans ce cadre',
+        selectFromFiles: 'Choisir un fichier',
+        url: 'URL de l\'image'
+      },
+      link: {
+        link: 'Lien',
+        insert: 'Insérer un lien',
+        unlink: 'Supprimer un lien',
+        edit: 'Modifier',
+        textToDisplay: 'Texte à afficher',
+        url: 'URL du lien'
+      },
+      video: {
+        video: 'Vidéo',
+        videoLink: 'Lien vidéo',
+        insert: 'Insérer une vidéo',
+        url: 'URL de la vidéo',
+        providers: '(YouTube, Vimeo, Vine, Instagram ou DailyMotion)'
+      },
+      table: {
+        table: 'Tableau'
+      },
+      hr: {
+        insert: 'Insérer une ligne horizontale de séparation'
+      },
+      style: {
+        style: 'Style',
+        normal: 'Normal',
+        blockquote: 'Citation',
+        pre: 'Code source',
+        h1: 'Titre 1',
+        h2: 'Titre 2',
+        h3: 'Titre 3',
+        h4: 'Titre 4',
+        h5: 'Titre 5',
+        h6: 'Titre 6'
+      },
+      lists: {
+        unordered: 'Liste à puces',
+        ordered: 'Liste numérotée'
+      },
+      options: {
+        help: 'Aide',
+        fullscreen: 'Plein écran',
+        codeview: 'Afficher le code HTML'
+      },
+      paragraph: {
+        paragraph: 'Paragraphe',
+        outdent: 'Diminuer le retrait',
+        indent: 'Augmenter le retrait',
+        left: 'Aligner à gauche',
+        center: 'Centrer',
+        right: 'Aligner à droite',
+        justify: 'Justifier'
+      },
+      color: {
+        recent: 'Dernière couleur sélectionnée',
+        more: 'Plus de couleurs',
+        background: 'Couleur de fond',
+        foreground: 'Couleur de police',
+        transparent: 'Transparent',
+        setTransparent: 'Définir la transparence',
+        reset: 'Restaurer',
+        resetToDefault: 'Restaurer la couleur par défaut'
+      },
+      shortcut: {
+        shortcuts: 'Raccourcis',
+        close: 'Fermer',
+        textFormatting: 'Mise en forme du texte',
+        action: 'Action',
+        paragraphFormatting: 'Mise en forme des paragraphes',
+        documentStyle: 'Style du document'
+      },
+      history: {
+        undo: 'Annuler la dernière action',
+        redo: 'Restaurer la dernière action annulée'
+      }
+
+    }
+  });
+})(jQuery);
