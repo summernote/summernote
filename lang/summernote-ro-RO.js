7adf666decf262914ccfb3a75320de7a277fcbb8
Razvan <raku_cmr@yahoo.com>
2014-02-02 21:20:53 +0200

    Romanian language

---
+++ lang/summernote-ro-RO.js
@@ -0,0 +1,102 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'ro-RO': {
+      font: {
+        bold: 'Îngroșat',
+        italic: 'Înclinat',
+        underline: 'Subliniat',
+        strike: 'Tăiat',
+        clear: 'Înlătură formatare font',
+        height: 'Înălțime rând',
+        size: 'Dimensiune font'
+      },
+      image: {
+        image: 'Imagine',
+        insert: 'Inserează imagine',
+        resizeFull: 'Redimensionează complet',
+        resizeHalf: 'Redimensionează 1/2',
+        resizeQuarter: 'Redimensionează 1/4',
+        floatLeft: 'Aliniere la stânga',
+        floatRight: 'Aliniere la dreapta',
+        floatNone: 'Fară aliniere',
+        dragImageHere: 'Trage o imagine aici',
+        selectFromFiles: 'Alege din fişiere',
+        url: 'URL imagine'
+      },
+      link: {
+        link: 'Link',
+        insert: 'Inserează link',
+        unlink: 'Înlătură link',
+        edit: 'Editează',
+        textToDisplay: 'Text ce va fi afişat',
+        url: 'Deschidere în fereastra nouă?'
+      },
+      video: {
+        video: 'Video',
+        videoLink: 'Link video',
+        insert: 'Inserează video',
+        url: 'URL video?',
+        providers: '(YouTube, Vimeo, Vine, Instagram, sau DailyMotion)'
+      },
+      table: {
+        table: 'Tabel'
+      },
+      hr: {
+        insert: 'Inserează o linie orizontală'
+      },
+      style: {
+        style: 'Stil',
+        normal: 'Normal',
+        blockquote: 'Citat',
+        pre: 'Preformatat',
+        h1: 'Titlu 1',
+        h2: 'Titlu 2',
+        h3: 'Titlu 3',
+        h4: 'Titlu 4',
+        h5: 'Titlu 5',
+        h6: 'Titlu 6'
+      },
+      lists: {
+        unordered: 'Listă neordonată',
+        ordered: 'Listă ordonată'
+      },
+      options: {
+        help: 'Ajutor',
+        fullscreen: 'Măreşte',
+        codeview: 'Sursă'
+      },
+      paragraph: {
+        paragraph: 'Paragraf',
+        outdent: 'Creşte identarea',
+        indent: 'Scade identarea',
+        left: 'Aliniere la stânga',
+        center: 'Aliniere centrală',
+        right: 'Aliniere la dreapta',
+        justify: 'Aliniere în bloc'
+      },
+      color: {
+        recent: 'Culoare recentă',
+        more: 'Mai multe  culori',
+        background: 'Culoarea fundalului',
+        foreground: 'Culoarea textului',
+        transparent: 'Transparent',
+        setTransparent: 'Setează transparent',
+        reset: 'Resetează',
+        resetToDefault: 'Revino la iniţial'
+      },
+      shortcut: {
+        shortcuts: 'Scurtături tastatură',
+        close: 'Închide',
+        textFormatting: 'Formatare text',
+        action: 'Acţiuni',
+        paragraphFormatting: 'Formatare paragraf',
+        documentStyle: 'Stil paragraf'
+      },
+      history: {
+        undo: 'Starea anterioară',
+        redo: 'Starea ulterioară'
+      }
+
+    }
+  });
+})(jQuery);
