9f799d1c3ffd79f1e704ec25ec939a988173b602
Eirik Krogstad <eirikkr@gmail.com>
2014-03-01 22:55:48 +0100

    Added norwegian bokmål translation (nb-NO)

---
+++ lang/summernote-nb-NO.js
@@ -0,0 +1,104 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'nb-NO': {
+      font: {
+        bold: 'Fet',
+        italic: 'Kursiv',
+        underline: 'Understrek',
+        strike: 'Gjennomstrek',
+        clear: 'Fjern formatering',
+        height: 'Linjehøyde',
+        name: 'Skrifttype',
+        size: 'Skriftstørrelse'
+      },
+      image: {
+        image: 'Bilde',
+        insert: 'Sett inn bilde',
+        resizeFull: 'Sett full størrelse',
+        resizeHalf: 'Sett halv størrelse',
+        resizeQuarter: 'Sett kvart størrelse',
+        floatLeft: 'Flyt til venstre',
+        floatRight: 'Flyt til høyre',
+        floatNone: 'Fjern flyt',
+        dragImageHere: 'Dra et bilde hit',
+        selectFromFiles: 'Velg fra filer',
+        url: 'Bilde-URL',
+        remove: 'Fjern bilde'
+      },
+      link: {
+        link: 'Lenke',
+        insert: 'Sett inn lenke',
+        unlink: 'Fjern lenke',
+        edit: 'Rediger',
+        textToDisplay: 'Visningstekst',
+        url: 'Til hvilken URL skal denne lenken peke?',
+        openInNewWindow: 'Åpne i nytt vindu'
+      },
+      video: {
+        video: 'Video',
+        videoLink: 'Videolenke',
+        insert: 'Sett inn video',
+        url: 'Video-URL',
+        providers: '(YouTube, Vimeo, Vine, Instagram eller DailyMotion)'
+      },
+      table: {
+        table: 'Tabell'
+      },
+      hr: {
+        insert: 'Sett inn horisontal linje'
+      },
+      style: {
+        style: 'Stil',
+        normal: 'Normal',
+        blockquote: 'Sitat',
+        pre: 'Kode',
+        h1: 'Overskrift 1',
+        h2: 'Overskrift 2',
+        h3: 'Overskrift 3',
+        h4: 'Overskrift 4',
+        h5: 'Overskrift 5',
+        h6: 'Overskrift 6'
+      },
+      lists: {
+        unordered: 'Punktliste',
+        ordered: 'Nummerert liste'
+      },
+      options: {
+        help: 'Hjelp',
+        fullscreen: 'Fullskjerm',
+        codeview: 'HTML-visning'
+      },
+      paragraph: {
+        paragraph: 'Avsnitt',
+        outdent: 'Tilbakerykk',
+        indent: 'Innrykk',
+        left: 'Venstrejustert',
+        center: 'Midtstilt',
+        right: 'Høyrejustert',
+        justify: 'Blokkjustert'
+      },
+      color: {
+        recent: 'Nylig valgt farge',
+        more: 'Flere farger',
+        background: 'Bakgrunnsfarge',
+        foreground: 'Skriftfarge',
+        transparent: 'Gjennomsiktig',
+        setTransparent: 'Sett gjennomsiktig',
+        reset: 'Nullstill',
+        resetToDefault: 'Nullstill til standard'
+      },
+      shortcut: {
+        shortcuts: 'Hurtigtaster',
+        close: 'Lukk',
+        textFormatting: 'Tekstformatering',
+        action: 'Handling',
+        paragraphFormatting: 'Avsnittsformatering',
+        documentStyle: 'Dokumentstil'
+      },
+      history: {
+        undo: 'Angre',
+        redo: 'Gjør om'
+      }
+    }
+  });
+})(jQuery);
