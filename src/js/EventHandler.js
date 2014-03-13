846f89e61349d9bc22c5a42bff81ba81682706fe
Park Hyunwoo <ez@smartstudy.co.kr>
2014-03-13 15:28:14 +0900

    Fix wrong handling in showImageDialog
    `string` means data is url, not file list. So it must be passed to
    `editor.insertImage`. And also, not `string` means it is file list. So
    they should be handled by `insertImages`.

--- src/js/EventHandler.js
+++ src/js/EventHandler.js
@@ -242,10 +242,10 @@ define([
 
           dialog.showImageDialog($editable, $dialog).then(function (data) {
             if (typeof data === 'string') {
-              insertImages($editable, data);
+              editor.insertImage($editable, data);
             } else {
               editor.restoreRange($editable);
-              editor.insertImage($editable, data);
+              insertImages($editable, data);
             }
           });
         } else if (sEvent === 'showVideoDialog') {
