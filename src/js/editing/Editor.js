4b61ed98df69b36c425576943b3edb4c806f2fba
Hendri Smit <hen3smit@gmail.com>
2014-03-24 16:15:41 +0100

    added support for html5 video tag with following video formats: theora (ogg), h.264 (mp4), vp8/vp9 (webm)

--- src/js/editing/Editor.js
+++ src/js/editing/Editor.js
@@ -81,7 +81,7 @@ define([
     /* jshint ignore:end */
 
     /**
-     * @param {jQuery} $editable 
+     * @param {jQuery} $editable
      * @param {WrappedRange} rng
      * @param {Number} nTabsize
      */
@@ -97,7 +97,7 @@ define([
 
     /**
      * handle tab key
-     * @param {jQuery} $editable 
+     * @param {jQuery} $editable
      * @param {Number} nTabsize
      * @param {Boolean} bShift
      */
@@ -150,7 +150,7 @@ define([
     this.insertVideo = function ($editable, sUrl) {
       recordUndo($editable);
 
-      // video url patterns(youtube, instagram, vimeo, dailymotion)
+      // video url patterns(youtube, instagram, vine, vimeo, dailymotion, mp4, ogg, webm)
       var ytRegExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
       var ytMatch = sUrl.match(ytRegExp);
 
@@ -166,37 +166,54 @@ define([
       var dmRegExp = /.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/;
       var dmMatch = sUrl.match(dmRegExp);
 
+      var mp4RegExp = /^.+.(mp4|m4v)$/;
+      var mp4Match = sUrl.match(mp4RegExp);
+
+      var oggRegExp = /^.+.(ogg|ogv)$/;
+      var oggMatch = sUrl.match(oggRegExp);
+
+      var webmRegExp = /^.+.(webm)$/;
+      var webmMatch = sUrl.match(webmRegExp);
+
       var $video;
       if (ytMatch && ytMatch[2].length === 11) {
         var youtubeId = ytMatch[2];
         $video = $('<iframe>')
+          .attr('frameborder', 0)
           .attr('src', '//www.youtube.com/embed/' + youtubeId)
           .attr('width', '640').attr('height', '360');
       } else if (igMatch && igMatch[0].length > 0) {
         $video = $('<iframe>')
+          .attr('frameborder', 0)
           .attr('src', igMatch[0] + '/embed/')
           .attr('width', '612').attr('height', '710')
           .attr('scrolling', 'no')
           .attr('allowtransparency', 'true');
       } else if (vMatch && vMatch[0].length > 0) {
         $video = $('<iframe>')
+          .attr('frameborder', 0)
           .attr('src', vMatch[0] + '/embed/simple')
           .attr('width', '600').attr('height', '600')
           .attr('class', 'vine-embed');
       } else if (vimMatch && vimMatch[3].length > 0) {
         $video = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen>')
+          .attr('frameborder', 0)
           .attr('src', '//player.vimeo.com/video/' + vimMatch[3])
           .attr('width', '640').attr('height', '360');
       } else if (dmMatch && dmMatch[2].length > 0) {
         $video = $('<iframe>')
+          .attr('frameborder', 0)
           .attr('src', '//www.dailymotion.com/embed/video/' + dmMatch[2])
+          .attr('width', '640').attr('height', '360');
+      } else if (mp4Match || oggMatch || webmMatch) {
+        $video = $('<video controls>')
+          .attr('src', sUrl)
           .attr('width', '640').attr('height', '360');
       } else {
         // this is not a known video link. Now what, Cat? Now what?
       }
 
       if ($video) {
-        $video.attr('frameborder', 0);
         range.create().insertNode($video[0]);
       }
     };
