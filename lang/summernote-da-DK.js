897f888ee6542e77d5fa6b0950d26a859acee566
Morten Lyng <morten_hau@hotmail.com>
2014-05-13 08:54:33 +0200

    summernote-da-DK.js
    Danish translation for summernote

---
+++ lang/summernote-da-DK.js
@@ -0,0 +1,104 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'da-DK': {
+      font: {
+        bold: 'Fed',
+        italic: 'Kursiv',
+        underline: 'Understreget',
+        strike: 'Genemstreget',
+        clear: 'Fjern formatering',
+        height: 'Højde',
+        size: 'Skriftstørrelse'
+      },
+      image: {
+        image: 'Billede',
+        insert: 'Indsæt billede',
+        resizeFull: 'Original størrelse',
+        resizeHalf: 'Halv størrelse',
+        resizeQuarter: 'Kvart størrelse',
+        floatLeft: 'Venstrestillet',
+        floatRight: 'Højrestillet',
+        floatNone: 'Fjern formatering',
+        dragImageHere: 'Træk billede hertil',
+        selectFromFiles: 'Vælg billed-fil',
+        url: 'Billede URL',
+		remove: 'Fjern billede'
+      },
+      link: {
+        link: 'Link',
+        insert: 'Indsæt link',
+        unlink: 'Fjern link',
+        edit: 'Rediger',
+        textToDisplay: 'Visningstekst',
+        url: 'Hvor skal linket pege hen?',
+        openInNewWindow: 'Åbn i nyt vindue'
+      },
+      video: {
+        video: 'Video',
+        videoLink: 'Video Link',
+        insert: 'Indsæt Video',
+        url: 'Video URL?',
+        providers: '(YouTube, Vimeo, Vine, Instagram, or DailyMotion)'
+      },
+      table: {
+        table: 'Tabel'
+      },
+      hr: {
+        insert: 'Indsæt horisontal linje'
+      },
+      style: {
+        style: 'Stil',
+        normal: 'Normal',
+        blockquote: 'Citat',
+        pre: 'Kode',
+        h1: 'Overskrift 1',
+        h2: 'Overskrift 2',
+        h3: 'Overskrift 3',
+        h4: 'Overskrift 4',
+        h5: 'Overskrift 5',
+        h6: 'Overskrift 6'
+      },
+      lists: {
+        unordered: 'Punktopstillet liste',
+        ordered: 'Nummereret liste'
+      },
+      options: {
+        help: 'Hjælp',
+        fullscreen: 'Fuld skærm',
+        codeview: 'HTML-Visning'
+      },
+      paragraph: {
+        paragraph: 'Afsnit',
+        outdent: 'Formindsk indryk',
+        indent: 'Forøg indryk',
+        left: 'Venstrestillet',
+        center: 'Centreret',
+        right: 'Højrestillet',
+        justify: 'Blokjuster'
+      },
+      color: {
+        recent: 'Nyligt valgt farve',
+        more: 'Flere farver',
+        background: 'Baggrund',
+        foreground: 'Forgrund',
+        transparent: 'Transparetn',
+        setTransparent: 'Sæt transparent',
+        reset: 'Nulstil',
+        resetToDefault: 'Gendan standardindstillinger'
+      },
+      shortcut: {
+        shortcuts: 'Genveje',
+        close: 'Luk',
+        textFormatting: 'Tekstformatering',
+        action: 'Handling',
+        paragraphFormatting: 'Afsnitsformatering',
+        documentStyle: 'Dokumentstil'
+      },
+      history: {
+        undo: 'Fortryd',
+        redo: 'Anuller fortryd'
+      }
+
+    }
+  });
+})(jQuery);
