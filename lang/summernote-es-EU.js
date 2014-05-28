8ed64358c1aade58e87bc46e7823f707d08661e8
Iñigo In The Cloud <soporte@gestudio.com>
2014-05-28 10:44:44 +0200

    Initial translation to Basque
    Basque, Euskera (es-EU, eu, bas) from Basque Country, Europe.

---
+++ lang/summernote-es-EU.js
@@ -0,0 +1,103 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'es-EU': {
+      font: {
+        name:'Tipografia', //Fuente
+        bold: 'Lodia', //Negrita
+        italic: 'Etzana', //Itálica
+        underline: 'Nabarmendu', //Subrayar
+        strikethrough: 'Marratu', //Tachar
+        clear: 'Estiloa kendu', //Quitar Estilo
+        height: 'Lerro altuera', //Alto de línea
+        size: 'Letren neurria' //Tamaño de la fuente
+      },
+      image: {
+        image: 'Irudia', //Imagen
+        insert: 'Irudi bat txertatu', // Insertar una imagen
+        resizeFull: 'Jatorrizko neurrira aldatu', //Redimensionar al tamaño original
+        resizeHalf: 'Erdira neurria aldatu', //Redimensionar a la mitad
+        resizeQuarter: 'Laurden batera neurria aldatu', //Redimensionar a un cuarto
+        floatLeft: 'Ezkerrean kokatu', //Flotar a la izquierda 
+        floatRight: 'Eskuinean kokatu', //Flotar a la derecha
+        floatNone: 'Kokapena ez ezarri', //No flotar
+        dragImageHere: 'Irudi bat ezarri hemen', //Arrastrar una imagen aquí
+        selectFromFiles: 'Zure fitxategi bat aukeratu', //Seleccionar desde los archivos
+        url: 'Irudiaren URL helbidea' //URL de la imagen
+      },
+      link: {
+        link: 'Esteka',//Link
+        insert: 'Esteka bat txertatu', //Insertar link
+        unlink: 'Esteka ezabatu', //Quitar link
+        edit: 'Editatu', //Editar
+        textToDisplay: 'Estekaren textua', //Texto para mostrar
+        url: 'Estekaren URL helbidea?', //Hacia que URL lleva el link?
+        openInNewWindow: 'Lehio berri batean ireki' //Abrir en una nueva ventana
+      },
+      video: {
+        video: 'Bideoa', //Video
+        videoLink: 'Bideoarako esteka', //Link del video
+        insert: 'Bideo berri bat txertatu', //Insertar video
+        url: 'Bideoaren URL helbidea?', //URL del video
+        providers: '(YouTube, Vimeo, Vine, Instagram, edo DailyMotion)' //(YouTube, Vimeo, Vine, Instagram, o DailyMotion)
+      },
+      table: {
+        table: 'Taula' //Tabla
+      },
+      hr: {
+        insert: 'Separazio marra horizontala txertatu' //Insertar línea horizontal
+      },
+      style: {
+        style: 'Estiloa', //Estilo
+        normal: 'Normal', //Normal
+        blockquote: 'Aipamena', //Cita
+        pre: 'Kodea', //Código
+        h1: '1. Titularra', //Título 1
+        h2: '2. Titularra',
+        h3: '3. Titularra',
+        h4: '4. Titularra',
+        h5: '5. Titularra',
+        h6: '6. Titularra'
+      },
+      lists: {
+        unordered: 'Zenbatu gabeko zerrenda', //Lista desordenada
+        ordered: 'Zerrenda zenbatua' //Lista ordenada
+      },
+      options: {
+        help: 'Laguntza', //Ayuda
+        fullscreen: 'Pantaila osoa', //Pantalla completa
+        codeview: 'Kodea ikusi' //Ver código fuente
+      },
+      paragraph: {
+        paragraph: 'Paragrafoa', //Párrafo
+        outdent: 'Tabulazio gutxiago', //Menos tabulación
+        indent: 'Tabulazio gehiago', //Más tabulación
+        left: 'Ezkerreak kokatu', //Alinear a la izquierda
+        center: 'Erdian kokatu', //Alinear al centro
+        right: 'Eskuinean kokatu', //Alinear a la derecha
+        justify: 'Justifikatu' //????????? Justificar
+      },
+      color: {
+        recent: 'Azken kolorea', //Último color
+        more: 'Kolore gehiago', //Más colores
+        background: 'Atzeko kolorea', // ?????? Color de fondo
+        foreground: 'Tipografi kolorea', //Color de fuente
+        transparent: 'Gardena', //Transparente
+        setTransparent: 'Gardendu', //????? Establecer transparente
+        reset: 'Hasierako egoerara bueltatu', // motzago, eta bueltatu edo bueltarazi????????     Restaurar
+        resetToDefault: 'Egoera originalera bueltatu' //   berdin!!! Restaurar por defecto
+      },
+      shortcut: {
+        shortcuts: 'Teklatuaren abantaiak', // ????????? Atajos de teclado
+        close: 'Itxi', //Cerrar
+        textFormatting: 'Textuaren formatua', //????? Formato de texto
+        action: 'Ekintza', //Acción
+        paragraphFormatting: 'Paragrafoaren formatua', //Formato de párrafo
+        documentStyle: 'Dokumentuaren estiloa' //Estilo de documento
+      },
+      history: {
+        undo: 'Desegin', //Deshacer
+        redo: 'Berregin' //Rehacer
+      }
+    }
+  });
+})(jQuery);
