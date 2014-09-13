668dbd395fdf7ad3e28bdd9e258ea748a9f443a6
Park Hyunwoo <ez@smartstudy.co.kr>
2014-09-13 10:50:38 +0900

    Update dist file

--- dist/summernote.js
+++ dist/summernote.js
@@ -6,7 +6,7 @@
  * Copyright 2013 Alan Hong. and outher contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2014-09-13T01:43Z
+ * Date: 2014-09-13T01:49Z
  */
 (function (factory) {
   /* global define */
