3a7014ad7cef6b9deb56fa1700b71cfeb0d4770d
Park Hyunwoo <ez@smartstudy.co.kr>
2014-10-24 16:12:58 +0900

    Embed youtube with wmode as  to prevent showing toolbar dialogs beneath videos

--- dist/summernote.js
+++ dist/summernote.js
@@ -6,7 +6,7 @@
  * Copyright 2013-2014 Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2014-10-16T20:18Z
+ * Date: 2014-10-24T07:11Z
  */
 (function (factory) {
   /* global define */
@@ -2850,7 +2850,7 @@
       if (ytMatch && ytMatch[1].length === 11) {
         var youtubeId = ytMatch[1];
         $video = $('<iframe>')
-          .attr('src', '//www.youtube.com/embed/' + youtubeId)
+          .attr('src', '//www.youtube.com/embed/' + youtubeId + '?wmode=opaque')
           .attr('width', '640').attr('height', '360');
       } else if (igMatch && igMatch[0].length) {
         $video = $('<iframe>')
