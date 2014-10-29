6d1b18d11e3a3fad25a92fd868bf495cf0de800e
hackerwins <susukang98@gmail.com>
2014-10-30 05:20:07 +0900

    Detach img node from body when image load failed. #687

--- dist/summernote.js
+++ dist/summernote.js
@@ -6,7 +6,7 @@
  * Copyright 2013-2014 Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2014-10-29T19:50Z
+ * Date: 2014-10-29T20:18Z
  */
 (function (factory) {
   /* global define */
@@ -2187,7 +2187,7 @@
         $('<img>').one('load', function () {
           deferred.resolve($(this));
         }).one('error abort', function () {
-          deferred.reject($(this));
+          deferred.reject($(this).detach());
         }).css({
           display: 'none'
         }).appendTo(document.body)
