f8ef1f87f8e5c59c74b78a8f7464b258c46a1a72
hackerwins <susukang98@gmail.com>
2015-01-17 16:15:04 +0900

    Fixed incorrect code style.

--- src/js/core/agent.js
+++ src/js/core/agent.js
@@ -8,24 +8,24 @@ define(['jquery'], function ($) {
      * @param {Value} [initialValue]
      */
     Array.prototype.reduce = function (callback) {
-        var t = Object(this), len = t.length >>> 0, k = 0, value;
-        if (arguments.length == 2) {
-            value = arguments[1];
-        } else {
-            while (k < len && ! k in t) {
-                k++; 
-            }
-            if (k >= len) {
-                throw new TypeError('Reduce of empty array with no initial value');
-            }
-            value = t[k++];
+      var t = Object(this), len = t.length >>> 0, k = 0, value;
+      if (arguments.length === 2) {
+        value = arguments[1];
+      } else {
+        while (k < len && !(k in t)) {
+          k++;
         }
-        for (; k < len; k++) {
-            if (k in t) {
-                value = callback(value, t[k], k, t);
-            }
+        if (k >= len) {
+          throw new TypeError('Reduce of empty array with no initial value');
         }
-        return value;
+        value = t[k++];
+      }
+      for (; k < len; k++) {
+        if (k in t) {
+          value = callback(value, t[k], k, t);
+        }
+      }
+      return value;
     };
   }
 
