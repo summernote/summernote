1bae4c8de7d82a600cf467bd60ca8fcf475f3edb
easylogic <cyberuls@gmail.com>
2015-02-07 04:16:30 +0900

    add jsduck

--- plugin/summernote-ext-hello.js
+++ plugin/summernote-ext-hello.js
@@ -12,9 +12,20 @@
   var tmpl = $.summernote.renderer.getTemplate();
   var editor = $.summernote.eventHandler.getEditor();
 
-  // add plugin
+  /**
+   * @class plugin.hello 
+   * 
+   * Hello Plugin  
+   */
   $.summernote.addPlugin({
-    name: 'hello', // name of plugin
+    /** @property {String} name name of plugin */
+    name: 'hello',
+    /** 
+     * @property {Object} buttons 
+     * @property {Function} hello   function to make button  
+     * @property {Function} helloDropdown   function to make button  
+     * @property {Function} helloImage   function to make button
+     */
     buttons: { // buttons
       hello: function () {
 
@@ -47,6 +58,12 @@
 
     },
 
+    /**
+     * @property {Object} events 
+     * @property {Function} events.hello  run function when button that has a 'hello' event name  fires click
+     * @property {Function} events.helloDropdown run function when button that has a 'helloDropdown' event name  fires click
+     * @property {Function} events.helloImage run function when button that has a 'helloImage' event name  fires click
+     */
     events: { // events
       hello: function (layoutInfo) {
         // Get current editable node
