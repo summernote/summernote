a7e17dab7f5fa8a4a51b1dce970cf4868c37d9ff
hackerwins <susukang98@gmail.com>
2015-04-22 00:45:08 +0900

    @easylogic's approach. using documentFragment.

--- src/js/core/range.js
+++ src/js/core/range.js
@@ -495,11 +495,11 @@ define([
         var self = this;
         var rng = this.wrapBodyInlineWithPara().deleteContents();
         var contentsContainer = $('<div></div>').html(markup)[0];
-        var childNodes = list.from(contentsContainer.childNodes);
+        var frag = document.createDocumentFragment();
+        var contents = list.from(contentsContainer.childNodes);
 
-        return $.map(childNodes.reverse(), function (childNode) {
-          return self.insertNode(childNode);
-        }).reverse();
+        this.insertNode(dom.appendChildNodes(frag, contents));
+        return contents;
       };
   
       /**
