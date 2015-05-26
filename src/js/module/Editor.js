2ce99a409b08cd173da6baa74a29821cec78d252
hackerwins <susukang98@gmail.com>
2015-05-26 22:27:10 +0900

    Remove TODO comments.

--- src/js/module/Editor.js
+++ src/js/module/Editor.js
@@ -796,7 +796,6 @@ define([
      * @return {Boolean}
      */
     this.isEmpty = function ($editable) {
-      // TODO test more empty condition.
       return dom.isEmpty($editable[0]) || dom.emptyPara === $editable.html();
     };
   };
