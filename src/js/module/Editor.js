be7e1aeae1ffa04e433d6eacef4cb1c0f64a0adf
Hackerwins <susukang98@gmail.com>
2015-06-24 00:31:14 +0900

    Move self to top and use inline condition.

--- src/js/module/Editor.js
+++ src/js/module/Editor.js
@@ -22,6 +22,7 @@ define([
    */
   var Editor = function (handler) {
 
+    var self = this;
     var style = new Style();
     var table = new Table();
     var typing = new Typing();
@@ -150,7 +151,6 @@ define([
       triggerOnChange($editable);
     };
 
-    var self = this;
     /**
      * @method beforeCommand
      * before command
@@ -482,9 +482,8 @@ define([
      */
     this.fontSize = function ($editable, value) {
       var rng = range.create();
-      var isCollapsed = rng.isCollapsed();
 
-      if (isCollapsed) {
+      if (rng.isCollapsed()) {
         var spans = style.styleNodes(rng);
         var firstSpan = list.head(spans);
 
