ead423b7ac8456ea007813d674a99b3848ee22cc
Hackerwins <susukang98@gmail.com>
2015-07-17 23:18:44 +0900

    Style and code cleaning for #1220.

--- dist/summernote.js
+++ dist/summernote.js
@@ -6,7 +6,7 @@
  * Copyright 2013-2015 Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2015-07-17T13:58Z
+ * Date: 2015-07-17T14:16Z
  */
 (function (factory) {
   /* global define */
