efc7884c195ed5d3836bf6e2a4a4724ee35ba869
Hackerwins <susukang98@gmail.com>
2015-07-25 04:07:34 +0900

    Hotfix: fix a bug that codeview not working without codemirror.

--- dist/summernote.js
+++ dist/summernote.js
@@ -6,7 +6,7 @@
  * Copyright 2013-2015 Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2015-07-24T17:43Z
+ * Date: 2015-07-24T19:04Z
  */
 (function (factory) {
   /* global define */
