8975b63871eff8c6c07dc7ad1b5a22766c07b693
Hackerwins <susukang98@gmail.com>
2015-07-25 11:27:40 +0900

    Update dist files.

--- dist/summernote.js
+++ dist/summernote.js
@@ -1,12 +1,12 @@
 /**
- * Super simple wysiwyg editor on Bootstrap v0.6.12
+ * Super simple wysiwyg editor on Bootstrap v0.6.13
  * http://summernote.org/
  *
  * summernote.js
  * Copyright 2013-2015 Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2015-07-24T19:08Z
+ * Date: 2015-07-25T02:27Z
  */
 (function (factory) {
   /* global define */
@@ -2381,7 +2381,7 @@
    */
   var defaults = {
     /** @property */
-    version: '0.6.12',
+    version: '0.6.13',
 
     /**
      * 
@@ -6563,7 +6563,7 @@
                    '<div class="title">' + lang.shortcut.shortcuts + '</div>' +
                    (agent.isMac ? tplShortcutTable(lang, options) : replaceMacKeys(tplShortcutTable(lang, options))) +
                    '<p class="text-center">' +
-                     '<a href="//summernote.org/" target="_blank">Summernote 0.6.12</a> · ' +
+                     '<a href="//summernote.org/" target="_blank">Summernote 0.6.13</a> · ' +
                      '<a href="//github.com/summernote/summernote" target="_blank">Project</a> · ' +
                      '<a href="//github.com/summernote/summernote/issues" target="_blank">Issues</a>' +
                    '</p>';
