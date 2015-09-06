8f60423b1dfeee75d88928af6ecafb3533cb4138
hackerwins <susukang98@gmail.com>
2015-09-06 12:56:38 +0900

    Rollback #3c0d567 for developing bootstrap version.

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,7 @@ require.config({
   baseUrl: 'src/js',
   paths: {
     jquery: '//code.jquery.com/jquery-2.1.4',
-    bootstrap: '//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap',
-    jui : ''
+    bootstrap: '//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap'
   },
   shim: {
     bootstrap: ['jquery']
@@ -17,18 +16,15 @@ require.config({
 
 require([
   'jquery',
+  'bootstrap',
   'summernote',
   'summernote/lite/settings',
-  'summernote/jui/settings',
   'summernote/bs3/settings'
-], function ($, summernote, lite, jui, bs3) {
+], function ($, bootstrap, summernote, lite, bs3) {
   // editor type setting
   switch ($('script[data-editor-type]').data('editor-type')) {
     case 'lite':
       $.summernote = $.extend($.summernote, lite);
-      break;
-    case 'jui':
-      $.summernote = $.extend($.summernote, jui);
       break;
     case 'bs3':
       $.summernote = $.extend($.summernote, bs3);
