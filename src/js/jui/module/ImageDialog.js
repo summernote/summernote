d35b7c752ef20c9ba00a0abcd95cad315efbbdc2
jinho park <cyberuls@gmail.com>
2015-09-10 16:04:48 +0900

    implments summernote for jui

---
+++ src/js/jui/module/ImageDialog.js
@@ -0,0 +1,158 @@
+define([
+  'summernote/base/core/key',
+  'summernote/base/core/async'
+], function (key, async) {
+  var ImageDialog = function (summernote) {
+    var self = this;
+    var ui = $.summernote.ui;
+
+    var $editor = summernote.layoutInfo.editor;
+    var options = summernote.options;
+
+    this.initialize = function () {
+      var $container = options.dialogsInBody ? $(document.body) : $editor;
+
+      var imageLimitation = '';
+      if (options.maximumImageFileSize) {
+        var unit = Math.floor(Math.log(options.maximumImageFileSize) / Math.log(1024));
+        var readableSize = (options.maximumImageFileSize / Math.pow(1024, unit)).toFixed(2) * 1 +
+                           ' ' + ' KMGTP'[unit] + 'B';
+        imageLimitation = '<small>Maximum file size : ' + readableSize + '</small>';
+      }
+
+      var body = '<div class="form-group note-group-select-from-files">' +
+                   '<label>Select from files</label>' +
+                   '<input class="note-image-input input" type="file" name="files" accept="image/*" multiple="multiple" />' +
+                   imageLimitation +
+                 '</div>' +
+                 '<div class="form-group">' +
+                   '<label>Image URL</label>' +
+                   '<input class="note-image-url input" type="text" />' +
+                 '</div>';
+      var footer = '<button href="#" class="btn focus note-image-btn disabled" disabled>Insert Image</button>';
+
+      $container.append(ui.dialog({
+        className: 'note-image-dialog',
+        title: 'Insert Image',
+        body: body,
+        footer: footer
+      }).render());
+
+      this.$dialog = $container.find('.note-image-dialog');
+    };
+
+    this.toggleBtn = function ($btn, isEnable) {
+      $btn.toggleClass('disabled', !isEnable);
+      $btn.attr('disabled', !isEnable);
+    };
+
+    this.bindEnterKey = function ($input, $btn) {
+      $input.on('keypress', function (event) {
+        if (event.keyCode === key.code.ENTER) {
+          $btn.trigger('click');
+        }
+      });
+    };
+
+    this.insertImages = function (files) {
+      var callbacks = options.callbacks;
+
+      // If onImageUpload options setted
+      if (callbacks.onImageUpload) {
+        summernote.triggerEvent('image.upload', [files]);
+      // else insert Image as dataURL
+      } else {
+        $.each(files, function (idx, file) {
+          var filename = file.name;
+          if (options.maximumImageFileSize && options.maximumImageFileSize < file.size) {
+            summernote.triggerEvent('image.upload.error', ['image.maximum.filesize.error']);
+          } else {
+            async.readFileAsDataURL(file).then(function (dataURL) {
+              summernote.invoke('editor.insertImage', [dataURL, filename]);
+            }).fail(function () {
+              summernote.triggerEvent('image.upload.error');
+            });
+          }
+        });
+      }
+    };
+
+    this.show = function () {
+      summernote.invoke('editor.saveRange');
+      this.showImageDialog().then(function (data) {
+        summernote.invoke('editor.restoreRange');
+
+        if (typeof data === 'string') {
+          // image url
+          summernote.invoke('editor.insertImage', [data]);
+        } else {
+          // array of files
+          self.insertImages(data);
+        }
+      }).fail(function () {
+        summernote.invoke('editor.restoreRange');
+      });
+    };
+
+    /**
+     * show image dialog
+     *
+     * @param {jQuery} $dialog
+     * @return {Promise}
+     */
+    this.showImageDialog = function () {
+      return $.Deferred(function (deferred) {
+        var $imageInput = self.$dialog.find('.note-image-input'),
+            $imageUrl = self.$dialog.find('.note-image-url'),
+            $imageBtn = self.$dialog.find('.note-image-btn');
+
+        self.$dialog.jui('off');
+        self.$dialog.jui('on', 'show', function () {
+          // Cloning imageInput to clear element.
+          $imageInput.replaceWith($imageInput.clone()
+            .on('change', function () {
+              deferred.resolve(this.files || this.value);
+              self.$dialog.jui('hide');
+            })
+            .val('')
+          );
+
+          $imageBtn.click(function (event) {
+            event.preventDefault();
+
+            deferred.resolve($imageUrl.val());
+            self.$dialog.jui('hide');
+          });
+
+          $imageUrl.on('keyup paste', function (event) {
+            var url;
+            
+            if (event.type === 'paste') {
+              url = event.originalEvent.clipboardData.getData('text');
+            } else {
+              url = $imageUrl.val();
+            }
+            
+            self.toggleBtn($imageBtn, url);
+          }).val('').trigger('focus');
+          self.bindEnterKey($imageUrl, $imageBtn);
+        });
+
+        self.$dialog.jui('off', 'hide');
+        self.$dialog.jui('on', 'hide', function () {
+          $imageInput.off('change');
+          $imageUrl.off('keyup paste keypress');
+          $imageBtn.off('click');
+
+          if (deferred.state() === 'pending') {
+            deferred.reject();
+          }
+        });
+
+        self.$dialog.jui('show');
+      });
+    };
+  };
+
+  return ImageDialog;
+});
