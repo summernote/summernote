41c47112c3ea89b437a9e9c1cd9003f69f7dff3b
hackerwins <susukang98@gmail.com>
2015-09-29 14:00:41 +0900

    Support for CommonJS. #1335.

--- src/js/intro.js
+++ src/js/intro.js
@@ -13,8 +13,11 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = factory(require('jquery'));
   } else {
-    // Browser globals: jQuery
+    // Browser globals
     factory(window.jQuery);
   }
 }(function ($) {
