bd96b0dd6cc62e7cc5ea04c57cf81cb3493f4f80
Alan Hong <alan.hong@daumkakao.com>
2015-12-04 17:57:18 +0900

    Update version and fixed #228

--- src/js/base/editing/Typing.js
+++ src/js/base/editing/Typing.js
@@ -70,8 +70,8 @@ define([
             dom.remove(anchor);
           });
 
-          // replace empty heading with P tag
-          if (dom.isHeading(nextPara) && dom.isEmpty(nextPara)) {
+          // replace empty heading or pre with P tag
+          if ((dom.isHeading(nextPara) || dom.isPre(nextPara)) && dom.isEmpty(nextPara)) {
             nextPara = dom.replace(nextPara, 'p');
           }
         }
