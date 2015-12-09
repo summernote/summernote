52ed82fc0bf4006069560587aaddd6ab48541476
Outsider <outsideris@gmail.com>
2015-12-09 16:29:25 +0900

    add language files on dist folder and uglify them
    to deploy language files on cdn.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,16 @@ module.exports = function (grunt) {
         banner: '/*! Summernote v<%=pkg.version%> | (c) 2013-2015 Alan Hong and other contributors | MIT license */\n'
       },
       all: {
-        files: { 'dist/summernote.min.js': ['dist/summernote.js'] }
+        files: [
+          { 'dist/summernote.min.js': ['dist/summernote.js'] },
+          {
+            expand: true,
+            cwd: 'dist/lang',
+            src: '**/*.js',
+            dest: 'dist/lang',
+            ext: '.min.js'
+          }
+        ]
       }
     },
 
@@ -211,6 +220,16 @@ module.exports = function (grunt) {
       travis: {
         src: 'test/coverage/**/lcov.info'
       }
+    },
+    clean: {
+      dist: ['dist']
+    },
+    copy: {
+      dist: {
+        files: [
+          {src: 'lang/*', dest: 'dist/'}
+        ]
+      }
     }
   });
 
@@ -233,7 +252,11 @@ module.exports = function (grunt) {
   grunt.registerTask('saucelabs-test', ['karma:saucelabs']);
 
   // dist: make dist files
-  grunt.registerTask('dist', ['build', 'jshint', 'karma:dist', 'uglify', 'recess', 'compress']);
+  grunt.registerTask('dist', [
+    'clean:dist',
+    'build', 'jshint', 'karma:dist',
+    'copy:dist', 'uglify', 'recess', 'compress'
+  ]);
 
   // default: server
   grunt.registerTask('default', ['server']);
