2062a8240d652676f7bcfae75f5a2a8f57526874
jinho park <cyberuls@gmail.com>
2015-12-11 11:40:19 +0900

    test enter key for ie8

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,17 @@
+var isIE8 = document.getElementById('start').getAttribute('data-browser') === 'ie8';
+var requireJquery = (isIE8) ? 'jquery1x'  : 'jquery';
+
+var jqueryLink = (isIE8) ? '//code.jquery.com/jquery-1.11.3' : '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery';
 require.config({
   baseUrl: 'src/js',
   paths: {
-    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery',
+    jquery: jqueryLink,
     bootstrap: '//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap',
     lang: '../../lang/summernote-ko-KR'
   },
   shim: {
-    bootstrap: ['jquery'],
-    lang: ['jquery']
+    bootstrap: [requireJquery],
+    lang: [requireJquery]
   },
   packages: [{
     name: 'summernote',
@@ -16,10 +20,8 @@ require.config({
   }]
 });
 
-require([
-  'jquery',
-  'summernote'
-], function ($) {
+
+require(['jquery', 'summernote'], function ($) {
   var requireByPromise = function (paths) {
     return $.Deferred(function (deferred) {
       require(paths, function () {
