471f27e12e17aa25cc03e48fc684195402193160
Outsider <outsideris@gmail.com>
2015-12-21 20:20:26 +0900

    add an initial e2e test

---
+++ test/e2e/initialization.test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'Initialization' : function (client) {
+    client
+      /* jshint -W106 */
+      .url(client.launch_url + '/initialization.html')
+      /* jshint +W106 */
+      .pause(1000);
+
+    client.expect.element('#summernote').to.be.present.before(1000);
+    client.expect.element('#summernote').to.be.not.visible;
+
+    client.end();
+  }
+};
