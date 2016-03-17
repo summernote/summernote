e757c027294cf84ce51f56e5224fc02de4c1604c
Guido Pinkas <guidopinkas@Guidos-Mac-Pro.local>
2016-03-17 08:40:20 +0100

    Revert "make Dom accessible in Context for plugins"
    This reverts commit 259e54bb53471164ad844007684c72c8f5ef636a.

--- src/js/base/Context.js
+++ src/js/base/Context.js
@@ -17,7 +17,6 @@ define([
     this.modules = {};
     this.layoutInfo = {};
     this.options = options;
-    this.dom = dom;
 
     /**
      * create layout and initialize modules and other resources
