b0d5a5af18b223acb63d1d660db50f2ad849a252
Justin Yoo <justin.yoo@aliencube.com>
2016-07-12 14:38:07 +1000

    Update .nuspec to fall back locating content files

--- nuget-build.cmd
+++ nuget-build.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 tools\nuget setApiKey <API_KEY> -ConfigFile tools\NuGet.Config
-tools\nuget pack summernote.nuspec -version <VERSION> -OutputDirectory build -Symbols
+tools\nuget pack summernote.nuspec -version <VERSION> -OutputDirectory build
