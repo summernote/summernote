50f60aff930d9e127a5209875e75b2dcc70665f2
Nuno Valente <nunomrvalente@gmail.com>
2016-10-17 13:55:54 +0100

    removed files added in last commit

--- meteor/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Tinytest.add('Instantiation', function (test) {
-  var editor = document.createElement('div');
-  document.body.appendChild(editor);
-  $(editor).summernote();
-
-  test.equal(typeof $(editor).code(), 'string', 'Instantiation');
-});
