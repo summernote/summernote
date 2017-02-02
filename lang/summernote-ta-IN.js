862fa0617a2c3e6645beeee8c310b7345fdc31f2
abuabdul <abuabdul86@hotmail.com>
2017-02-02 22:45:19 +0530

    Add Tamil language support for summernote editor

---
+++ lang/summernote-ta-IN.js
@@ -0,0 +1,106 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'ta-IN': {
+      font: {
+        bold: 'தடித்த',
+        italic: 'சாய்வு',
+        underline: 'அடிக்கோடு',
+        clear: 'நீக்கு',
+        height: 'எழுத்துரு உயரம்',
+        name: 'எழுத்துரு பெயர்',
+        strikethrough: 'குறுக்குக் கோடு',
+        size: 'எழுத்துரு அளவு',
+        superscript: 'மேல் ஒட்டு',
+        subscript: 'கீழ் ஒட்டு'	
+      },
+      image: {
+        image: 'உருவப்படம்',
+        insert: 'படத்தை செருகு',
+        resizeFull: 'பட முழு அளவை',
+        resizeHalf: 'பட அரை அளவை',
+        resizeQuarter: 'பட கால் அளவை',
+        floatLeft: 'இடப்பக்கம் இழு',
+        floatRight: 'வலப்பக்கம் இழு',
+        floatNone: 'நிலை நிறுத்து',
+        dragImageHere: 'படத்தை இங்கே வை',
+        selectFromFiles: 'கோப்புகளை தேர்வு செய்',
+        url: 'இணையதள முகவரி',
+        remove: 'படத்தை நீக்கு'
+      },
+      video: {
+        video: 'காணொளி',
+        videoLink: 'காணொளி இணைப்பு',
+        insert: 'காணொளியை செருகு',
+        url: 'இணையதள முகவரி',
+        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion ou Youku)'
+      },
+      link: {
+        link: 'இணைப்பு',
+        insert: 'இணைப்பை செருகு',
+        unlink: 'இணைப்பை நீக்கு',
+        edit: 'இணைப்பை தொகு',
+        textToDisplay: 'காட்சி வாசகம்',
+        url: 'இணையதள முகவரி',
+        openInNewWindow: 'புதிய சாளரத்தில் திறக்க'
+      },
+      table: {
+        table: 'அட்டவணை'
+      },
+      hr: {
+        insert: 'கிடைமட்ட கோடு'
+      },
+      style: {
+        style: 'தொகுப்பு',
+        p: 'பத்தி',
+        blockquote: 'மேற்கோள்',
+        pre: 'குறிமுறை தளம்',
+        h1: 'தலைப்பு 1',
+        h2: 'தலைப்பு 2',
+        h3: 'தலைப்பு 3',
+        h4: 'தலைப்பு 4',
+        h5: 'தலைப்பு 5',
+        h6: 'தலைப்பு 6'
+      },
+      lists: {
+        unordered: 'வரிசையிடாத',
+        ordered: 'வரிசையிட்ட'
+      },
+      options: {
+        help: 'உதவி',
+        fullscreen: 'முழு திரை',
+        codeview: 'நிரலாக்க காட்சி'
+      },
+      paragraph: {
+        paragraph: 'பத்தி',
+        outdent: 'வெளித்தள்ளு',
+        indent: 'உள்ளே தள்ளு',
+        left: 'இடது சீரமைப்பு',
+        center: 'நடு சீரமைப்பு',
+        right: 'வலது சீரமைப்பு',
+        justify: 'நேர்த்தி செய்'
+      },
+      color: {
+        recent: 'அண்மை நிறம்',
+        more: 'மேலும்',
+        background: 'பின்புல நிறம்',
+        foreground: 'முன்புற நிறம்',
+        transparent: 'தெளிமையான',
+        setTransparent: 'தெளிமையாக்கு',
+        reset: 'மீட்டமைக்க',
+        resetToDefault: 'இயல்புநிலைக்கு மீட்டமை'
+      },
+      shortcut: {
+        shortcuts: 'குறுக்குவழி',
+        close: 'மூடு',
+        textFormatting: 'எழுத்து வடிவமைப்பு',
+        action: 'செயல்படுத்து',
+        paragraphFormatting: 'பத்தி வடிவமைப்பு',
+        documentStyle: 'ஆவண பாணி'
+      },
+      history: {
+        undo: 'மீளமை',
+        redo: 'மீண்டும்'
+      }
+    }
+  });
+})(jQuery);
