186b1c53296c7da5fb6a43e7c174d5d5da216a9e
Outsider <outsideris@gmail.com>
2017-10-04 23:52:57 +0900

    add an initial e2e test

--- test/e2e/static-server.js
+++ test/e2e/static-server.js
@@ -1,13 +1,13 @@
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveStatic = require('serve-static')
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
 
 var serve = serveStatic('./', {'index': ['index.html']});
 
 // Create server
 var server = http.createServer(function onRequest (req, res) {
-  serve(req, res, finalhandler(req, res))
-})
+  serve(req, res, finalhandler(req, res));
+});
 
 // Listen
 server.listen(3000);
