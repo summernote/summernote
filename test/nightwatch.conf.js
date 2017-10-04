b3e7d271e42d8f0f53851a0ef560aafe458e3bf7
Outsider <outsideris@gmail.com>
2017-10-05 03:52:30 +0900

    configure to run e2e test with saucelabs on TravisCI

--- test/nightwatch.conf.js
+++ test/nightwatch.conf.js
@@ -1,6 +1,6 @@
 /* jshint ignore:start */
 module.exports = (() => {
-  return {
+  const config =  {
     src_folders: ['test/e2e'],
     output_folder: 'test/reports',
 
@@ -24,7 +24,70 @@ module.exports = (() => {
         },
         filter: 'test/e2e/**/*.test.js',
       },
-    },
+      ie9: {
+        desiredCapabilities: {
+          browserName: 'internet explorer',
+          platform: 'Windows 7',
+          version: '9.0',
+        },
+      },
+      ie10: {
+        desiredCapabilities: {
+          browserName: 'internet explorer',
+          platform: 'Windows 8',
+          version: '10.0',
+        },
+      },
+      ie11: {
+        desiredCapabilities: {
+          browserName: 'internet explorer',
+          platform: 'Windows 8.1',
+          version: '11.0',
+        },
+      },
+      ie_edge: {
+        desiredCapabilities: {
+          browserName: 'microsoftedge',
+          platform: 'Windows 10',
+          version: 'latest',
+        },
+      },
+      chrome: {
+        desiredCapabilities: {
+          browserName: 'chrome',
+          platform: 'Windows 8',
+          version: 'latest',
+        },
+      },
+      firefox: {
+        desiredCapabilities: {
+          browserName: 'firefox',
+          platform: 'Windows 8',
+          version: 'latest',
+        },
+      },
+      safari: {
+        desiredCapabilities: {
+          browserName: 'safari',
+          platform: 'OS X 10.10',
+          version: 'latest',
+        },
+      },
+    }
   };
+
+  if (process.env.TRAVIS) {
+    config.test_settings.default = {
+      silent: true,
+      selenium_host: 'ondemand.saucelabs.com',
+      selenium_port: 80,
+      username: process.env.SAUCE_USERNAME,
+      access_key: process.env.SAUCE_ACCESS_KEY,
+      build: 'build-' + process.env.TRAVIS_JOB_NUMBER,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    };
+  }
+
+  return config;
 })();
 /* jshint ignore:end */
