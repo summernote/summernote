72840a8d75a48c420dd0f93d149ec300f5776fd3
hackerwins <susukang98@gmail.com>
2018-01-28 16:40:41 +0900

    Update bs4-beta to bs4 and Remove code tag from styles.

--- src/js/bs4/settings.js
+++ src/js/bs4/settings.js
@@ -121,7 +121,7 @@ $.summernote = $.extend($.summernote, {
     styleTags: [
       'p',
       { title: 'Blockquote', tag: 'blockquote', className: 'blockquote', value: 'blockquote' },
-      'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+      'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
     ],
 
     fontNames: [
