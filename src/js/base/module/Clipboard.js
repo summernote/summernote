c8bdb7587337d10b52ad849576c3038cd73a7b88
shikun <queenask@hotmail.com>
2018-03-29 20:32:38 +0800

    pasteImg

--- src/js/base/module/Clipboard.js
+++ src/js/base/module/Clipboard.js
@@ -19,7 +19,13 @@ export default class Clipboard {
     const clipboardData = event.originalEvent.clipboardData;
     if (clipboardData && clipboardData.items && clipboardData.items.length) {
       // paste img file
-      const item = clipboardData.items.length > 1 ? clipboardData.items[1] : lists.head(clipboardData.items);
+      if (clipboardData.items.length > 1) {
+        const item = clipboardData.items[1];
+      } else {
+        const item = lists.head(clipboardData.items);
+      }
+
+      // const item = lists.head(clipboardData.items);
       if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
         this.context.invoke('editor.insertImagesOrCallback', [item.getAsFile()]);
       }
