816a09df41c5680459382db6950b36e21ccf67e9
Park Hyunwoo <ez.amiryo@gmail.com>
2018-11-20 02:18:02 +0900

    Add color options for color buttons

--- src/js/bs4/settings.js
+++ src/js/bs4/settings.js
@@ -158,6 +158,11 @@ $.summernote = $.extend($.summernote, {
       ['Rosewood', 'Cinnamon', 'Olive', 'Parsley', 'Tiber', 'Midnight Blue', 'Valentino', 'Loulou']
     ],
 
+    colorButton: {
+      foreColor: '#000000',
+      backColor: '#FFFF00'
+    },
+
     lineHeights: ['1.0', '1.2', '1.4', '1.5', '1.6', '1.8', '2.0', '3.0'],
 
     tableClassName: 'table table-bordered',
