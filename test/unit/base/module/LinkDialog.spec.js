112751fe9a3580fbb09b431498aba5470208ed7d
easylogic <cyberuls@gmail.com>
2018-11-22 20:37:51 +0900

    Update test code

--- test/unit/base/module/LinkDialog.spec.js
+++ test/unit/base/module/LinkDialog.spec.js
@@ -38,6 +38,7 @@ describe('LinkDialog', () => {
   describe('LinkDialog', () => {
     it('should check new window when target=_blank', () => {
       range.createFromNode($editable.find('a')[0]).normalize().select();
+      context.invoke('editor.setLastRange')      
       dialog.show();
 
       var checked = dialog.$dialog
@@ -48,6 +49,7 @@ describe('LinkDialog', () => {
 
     it('should uncheck new window without target=_blank', () => {
       range.createFromNode($editable.find('a')[1]).normalize().select();
+      context.invoke('editor.setLastRange')
       dialog.show();
 
       var checked = dialog.$dialog.find('#sn-checkbox-open-in-new-window').is(':checked');
