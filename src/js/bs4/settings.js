cbab0c0ad2ac66800d979a13751f26d37f0f49c3
Park Hyunwoo <ez.amiryo@gmail.com>
2018-11-30 21:17:44 +0900

    Rollback styleTags of Summernote 4

--- src/js/bs4/settings.js
+++ src/js/bs4/settings.js
@@ -5,3 +5,9 @@ import '../base/settings.js';
 $.summernote = $.extend($.summernote, {
   ui: ui,
 });
+
+$.summernote.options.styleTags = [
+  'p',
+  { title: 'Blockquote', tag: 'blockquote', className: 'blockquote', value: 'blockquote' },
+  'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+];
