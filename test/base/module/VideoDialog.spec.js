6eb2d6be035c3ae48ff1f2bc64290e646cc16bcd
lostship <418181589@qq.com>
2020-04-30 12:13:25 +0800

    Update VideoDialog.spec.js
    The qq video url pattern has been changed from 'https://v.qq.com/iframe/player.html?vid=' to 'https://v.qq.com/txp/iframe/player.html?vid=', insert '/txp' in the path.

--- test/base/module/VideoDialog.spec.js
+++ test/base/module/VideoDialog.spec.js
@@ -48,9 +48,9 @@ describe('VideoDialog', () => {
         '//instagram.com/p/Bi9cbsxjn-F/embed/');
       // v.qq.com
       expectUrl('http://v.qq.com/cover/6/640ewqy2v071ppd.html?vid=f0196y2b2cx',
-        '//v.qq.com/iframe/player.html?vid=f0196y2b2cx&amp;auto=0');
+        '//v.qq.com/txp/iframe/player.html?vid=f0196y2b2cx&amp;auto=0');
       expectUrl('http://v.qq.com/x/page/p0330y279lm.html',
-        '//v.qq.com/iframe/player.html?vid=p0330y279lm&amp;auto=0');
+        '//v.qq.com/txp/iframe/player.html?vid=p0330y279lm&amp;auto=0');
       // Facebook
       expectUrl('https://www.facebook.com/Engineering/videos/631826881803/',
         '//www.facebook.com/plugins/video.php?href=www.facebook.com%2FEngineering%2Fvideos%2F631826881803');
