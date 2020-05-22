baafdf4a522d523da0736b2790fb090f0f997b14
Asjid Kalam <asjid.kalam@gmail.com>
2020-05-22 16:04:50 +0530

    Fixed XSS - summernote.js

--- dist/summernote.js
+++ dist/summernote.js
@@ -1980,7 +1980,19 @@ var isTextarea = makePredByNodeName('TEXTAREA');
 
 function dom_value($node, stripLinebreaks) {
   var val = isTextarea($node[0]) ? $node.val() : $node.html();
-
+  
+  // Cross Site Scripting Mitigation
+  String.prototype.escape = function() {
+    var tagsToReplace = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;'
+    };
+    return this.replace(/[&<>]/g, function(tag) {
+        return tagsToReplace[tag] || tag;
+    });
+  };
+  val = val.escape();
   if (stripLinebreaks) {
     return val.replace(/[\n\r]/g, '');
   }
