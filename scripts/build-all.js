65dd220990735cd650df03668caec10728fb9dfe
Park Hyunwoo <ez.amiryo@gmail.com>
2024-10-13 23:34:21 +0900

    Fix lint and remove obsoleted files

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -1,5 +1,5 @@
 import { build } from 'vite';
-import { configs, banners } from '../vite.config.js';
+import { configs } from '../vite.config.js';
 
 // build every files by iteratinge all styles
 for (const [index, [style, config]] of Object.entries(configs).entries()) {
@@ -9,14 +9,14 @@ for (const [index, [style, config]] of Object.entries(configs).entries()) {
   }
 
   // minified build
-  config.build.lib.fileName = (format, entryName) => { return `${entryName}.min.js` };
+  config.build.lib.fileName = (format, entryName) => { return `${entryName}.min.js`; };
   config.build.rollupOptions.output.assetFileNames = `summernote-${style}.min.[ext]`;
   await build(config);
 
   // non-minified build
   config.build.minify = false;
   config.build.terserOptions = { compress: false, mangle: false };
-  config.build.lib.fileName = (format, entryName) => { return `${entryName}.js` };
+  config.build.lib.fileName = (format, entryName) => { return `${entryName}.js`; };
   config.build.rollupOptions.output.assetFileNames = `summernote-${style}.[ext]`;
   await build(config);
 }
